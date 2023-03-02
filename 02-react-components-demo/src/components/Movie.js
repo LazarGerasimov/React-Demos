f017b79be5f943f9e5335e28d6f0ac8e7275c3aa
Laz <lazgerasimov@gmail.com>
2023-03-02 16:04:19 +0200

    movie comp

--- 02-react-components-demo/src/components/Movie.js
+++ 02-react-components-demo/src/components/Movie.js
@@ -3,7 +3,14 @@
 const Movie = (props) => {
     console.log(props.cast)
     return (
-        <article>{props.title}</article>
+        <article>
+            <h3>{props.title}</h3>
+            <h5>Year: {props.year}</h5>
+            <ul type="none"> Cast: 
+                <li>{props.cast[0]}</li>
+                <li>{props.cast[1]}</li>
+            </ul>
+        </article>
     )
 };
 
