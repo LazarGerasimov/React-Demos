cdb18335eef8aa3ec145cc4f47f0ecc690a1dfa0
Laz <lazgerasimov@gmail.com>
2023-03-02 16:15:37 +0200

    movie list comp

--- 02-react-components-demo/src/App.js
+++ 02-react-components-demo/src/App.js
@@ -1,35 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Movie from './components/Movie';
+import MovieList from './components/MovieList';
 
 function App() {
 
-    const movies = [
-        { title: 'Man of Steel', year: '2015', cast: ['Henry Cavill', 'Diane Lane', 'Russell Crowe'] },
-        { title: 'Harry Potter', year: '2004', cast: ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'] },
-        { title: 'Lord of the Rings', year: '2002', cast: ['Elijah Wood', 'Viggo Mortensen', 'Sean Astin'] },
-    ]
+    
 
     return (
         <div className="App">
             <h1>Movie List</h1>
-            <Movie
-                title={movies[0].title}
-                year={movies[0].year}
-                cast={movies[0].cast}
-            />
-
-            <Movie
-                title={movies[1].title}
-                year={movies[1].year}
-                cast={movies[1].cast}
-            />
-
-            <Movie
-                title={movies[2].title}
-                year={movies[2].year}
-                cast={movies[2].cast}
-            />
+            <MovieList />
         </div>
     )
 }
---
+++ 02-react-components-demo/src/components/MovieList.js
@@ -0,0 +1,36 @@
+import Movie from "./Movie"
+
+
+
+const MovieList = (props) => {
+
+    const movies = [
+        { title: 'Man of Steel', year: '2015', cast: ['Henry Cavill', 'Diane Lane', 'Russell Crowe'] },
+        { title: 'Harry Potter', year: '2004', cast: ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'] },
+        { title: 'Lord of the Rings', year: '2002', cast: ['Elijah Wood', 'Viggo Mortensen', 'Sean Astin'] },
+    ]
+
+    return (
+            <div className="movieList">
+                <Movie
+                title={movies[0].title}
+                year={movies[0].year}
+                cast={movies[0].cast}
+            />
+
+            <Movie
+                title={movies[1].title}
+                year={movies[1].year}
+                cast={movies[1].cast}
+            />
+
+            <Movie
+                title={movies[2].title}
+                year={movies[2].year}
+                cast={movies[2].cast}
+            />
+            </div>
+    )
+}
+
+export default MovieList;
