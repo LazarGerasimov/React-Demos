654d016161092056d3619fdf518665da537b93e5
Laz <lazgerasimov@gmail.com>
2023-03-02 14:53:11 +0200

    props

--- 01. React JSX/build/app.js
+++ 01. React JSX/build/app.js
@@ -7,18 +7,23 @@ var root = ReactDOM.createRoot(rootElement);
 // const secondHeadingElement = React.createElement('h2', {}, 'Heading content 2');
 // const headerElement = React.createElement('header', {}, headingElement, secondHeadingElement);
 
+var Heading = function Heading(props) {
+    return React.createElement(
+        "h1",
+        { className: "heading" },
+        "Heading ",
+        props.name
+    );
+};
+
 var headerElement = React.createElement(
-    'header',
+    "header",
     null,
+    React.createElement(Heading, { name: "One" }),
     React.createElement(
-        'h1',
+        "h2",
         null,
-        'Heading One'
-    ),
-    React.createElement(
-        'h2',
-        null,
-        'Heading Two'
+        "Heading Two"
     )
 );
 
--- 01. React JSX/src/app.js
+++ 01. React JSX/src/app.js
@@ -8,9 +8,13 @@ const root = ReactDOM.createRoot(rootElement);
 // const secondHeadingElement = React.createElement('h2', {}, 'Heading content 2');
 // const headerElement = React.createElement('header', {}, headingElement, secondHeadingElement);
 
+const Heading = (props) => {
+    return <h1 className="heading">Heading {props.name}</h1>
+}
+
 const headerElement = ( 
     <header>
-        <h1>Heading One</h1>
+        <Heading name="One" />
         <h2>Heading Two</h2>    
     </header>
 );
