5f03dff5009652609f33d08590ba2cc12ee43ab5
Laz <lazgerasimov@gmail.com>
2023-03-02 15:53:57 +0200

    arr comp

--- 02-react-components-demo/src/App.js
+++ 02-react-components-demo/src/App.js
@@ -3,17 +3,35 @@ import './App.css';
 import Movie from './components/Movie';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Movie List</h1>
-      <Movie 
-        title="Man of Steel" 
-        year="2015"
-        cast={['Henry Cavill', 'Diane Lane', 'Russell Crowe']}
-        isNew={false}
-      />
-    </div>
-  )
+
+    const movies = [
+        { title: 'Man of Steel', year: '2015', cast: ['Henry Cavill', 'Diane Lane', 'Russell Crowe'] },
+        { title: 'Harry Potter', year: '2004', cast: ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'] },
+        { title: 'Lord of the Rings', year: '2002', cast: ['Elijah Wood', 'Viggo Mortensen', 'Sean Astin'] },
+    ]
+
+    return (
+        <div className="App">
+            <h1>Movie List</h1>
+            <Movie
+                title={movies[0].title}
+                year={movies[0].year}
+                cast={movies[0].cast}
+            />
+
+            <Movie
+                title={movies[1].title}
+                year={movies[1].year}
+                cast={movies[1].cast}
+            />
+
+            <Movie
+                title={movies[2].title}
+                year={movies[2].year}
+                cast={movies[2].cast}
+            />
+        </div>
+    )
 }
 
 export default App;
