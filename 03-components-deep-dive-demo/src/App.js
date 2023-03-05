bf2211f4d7a1eee53be0ef1d6f973a31e5e1c1db
Laz <lazgerasimov@gmail.com>
2023-03-05 17:29:59 +0200

    .

--- 03-components-deep-dive-demo/src/App.js
+++ 03-components-deep-dive-demo/src/App.js
@@ -1,8 +1,15 @@
+import { MovieList } from './components/MovieList';
+import {movies} from './movies'
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-     
+      <h1>Movie Collection</h1>
+      <MovieList movies={movies.slice(0, 10)}/>
     </div>
   );
 }
---
+++ 03-components-deep-dive-demo/src/components/MovieList.js
@@ -0,0 +1,6 @@
+
+
+
+export function MovieList({movies}) { 
+    return null;
+}
