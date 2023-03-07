bb65cd032d1772a03f1af9475dbdb04347e5b424
Laz <lazgerasimov@gmail.com>
2023-03-07 12:37:09 +0000

    .

--- 03-components-deep-dive-demo/src/components/MovieList.js
+++ 03-components-deep-dive-demo/src/components/MovieList.js
@@ -1,4 +1,5 @@
 import { Movie } from "./Movie";
+import {createElement} from 'react';
 
 
 
@@ -6,16 +7,24 @@ export function MovieList({movies}) {
 
     // const firstMovie = movies[0];
 
-    return [
-        // <Movie 
-        // title={firstMovie.title}
-        // year={firstMovie.year}
-        // plot={firstMovie.plot}
-        // posterUrl={firstMovie.posterUrl}
-        // director={firstMovie.director}
-        // />
-        <Movie {...movies[0]} />,
-        <Movie {...movies[1]} />,
-        <Movie {...movies[2]} />
-    ]
+    const movieElements = [];
+    for (const movie of movies) {
+        movieElements.push(createElement(Movie, movie))
+    }
+
+    // return [
+    //     // <Movie 
+    //     // title={firstMovie.title}
+    //     // year={firstMovie.year}
+    //     // plot={firstMovie.plot}
+    //     // posterUrl={firstMovie.posterUrl}
+    //     // director={firstMovie.director}
+    //     // />
+
+    //     // <Movie {...movies[0]} />,
+    //     // <Movie {...movies[1]} />,
+    //     // <Movie {...movies[2]} />
+    // ]
+
+    return movieElements;
 }
