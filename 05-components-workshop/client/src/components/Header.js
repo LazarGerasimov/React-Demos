6db88e6fdcdf5da3aca513339023be2e92d78b56
Laz <lazgerasimov@gmail.com>
2023-03-12 21:41:14 +0000

    add Header/Footer

---
+++ 05-components-workshop/client/src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+
+export function Header() {
+    return (
+        <header class="header">
+            <div class="logo">
+                <span class="course">React Course - June 2022</span>
+                <span class="description">User List Demo</span>
+            </div>
+        </header>
+    )
+}
