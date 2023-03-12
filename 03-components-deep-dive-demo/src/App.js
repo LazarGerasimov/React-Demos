337f36959ddf5cc366dbc4c8783a8dcee26901df
Laz <lazgerasimov@gmail.com>
2023-03-12 16:53:07 +0000

    fetching json data

--- 03-components-deep-dive-demo/src/App.js
+++ 03-components-deep-dive-demo/src/App.js
@@ -1,31 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MovieList } from './components/MovieList';
-import { movies as movieData } from './movies'
+// import { movies as movieData } from './movies'
+
 
 
 function App() {
 
-  const [movies, setMovies] = useState(movieData);
+    const [movies, setMovies] = useState([]);
 
-  const onMovieDelete = (id) => {
-    setMovies(oldMovies => oldMovies.filter(x => x.id !== id));
-  }
+    useEffect(() => {
+        fetch(`http://localhost:3000/movies2.json`)
+            .then(res => res.json())
+            .then(moviesData => {
+                console.log(moviesData);
+                setMovies(moviesData.movies);
+            })
+    })
 
-  const onMovieSelect = (id) => {
-    setMovies(allMovies => allMovies.map(m => ({...m, selected: m.id === id})));
-  }
+    const onMovieDelete = (id) => {
+        setMovies(oldMovies => oldMovies.filter(x => x.id !== id));
+    }
+
+    const onMovieSelect = (id) => {
+        setMovies(allMovies => allMovies.map(m => ({ ...m, selected: m.id === id })));
+    }
 
 
-  return (
-    <div className="App">
-      <h1>Movie Collection</h1>
-      <MovieList 
-      movies={movies} 
-      onMovieDelete={onMovieDelete}
-      onMovieSelect={onMovieSelect}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Movie Collection</h1>
+            <MovieList
+                movies={movies}
+                onMovieDelete={onMovieDelete}
+                onMovieSelect={onMovieSelect}
+            />
+        </div>
+    );
 }
 
 export default App;
