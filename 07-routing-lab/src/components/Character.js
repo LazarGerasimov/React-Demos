3672815efee48e5e4ff2e2c698121b5d233b575e
Laz <lazgerasimov@gmail.com>
2023-03-19 19:36:08 +0000

    navigate back button

--- 07-routing-lab/src/components/Character.js
+++ 07-routing-lab/src/components/Character.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 const baseUrl = 'https://swapi.dev/api';
 
 export function Character() {
 
     const { characterId } = useParams(); // gets characterId from App.js -> Routes -> route with :characterId
+
     const [character, setCharacter] = useState({});
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`${baseUrl}/people/${characterId}`)
@@ -17,10 +20,17 @@ export function Character() {
 
     }, [characterId])
 
+    const onBackButtonClick = () => {
+        // navigate(-1); // go back to the previous page
+        navigate('/characters');
+    }
+
     return (
         <>
             <h1>Character Page</h1>
             <h2>{character.name}</h2>
+            <button onClick={onBackButtonClick}>Back</button>
+
         </>
     )
 }
