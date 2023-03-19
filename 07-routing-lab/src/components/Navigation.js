61a27b1acf4e563154757a5513f47887261e9a1b
Laz <lazgerasimov@gmail.com>
2023-03-19 20:11:24 +0000

    add NavLink

--- 07-routing-lab/src/components/Navigation.js
+++ 07-routing-lab/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import styles from './Navigation.module.css'
 import { Link } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 export const Navigation = () => {
     return (
@@ -8,6 +9,11 @@ export const Navigation = () => {
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/about'}>About</Link></li>
                 <li><Link to={'/characters'}>Characters</Link></li>
+
+                {/* <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'white'})} to={'/characters'}>Characters</NavLink></li>  */}
+                {/* <li><NavLink className={({isActive}) => isActive ? 'nav-active' : undefined} to={'/characters'}>Characters</NavLink></li>  */}
+                {/* can set dynamic styles, usually for Navigation */}
+
             </ul>
         </nav>
     )
