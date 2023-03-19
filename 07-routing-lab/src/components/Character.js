f4b61d1cae6ee3106a36cf6f0d816750bf72b3ee
Laz <lazgerasimov@gmail.com>
2023-03-19 19:32:46 +0000

    fetch characters, set state

--- 07-routing-lab/src/components/Character.js
+++ 07-routing-lab/src/components/Character.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
+const baseUrl = 'https://swapi.dev/api';
 
 export function Character() {
 
     const { characterId } = useParams(); // gets characterId from App.js -> Routes -> route with :characterId
+    const [character, setCharacter] = useState({});
+
+    useEffect(() => {
+        fetch(`${baseUrl}/people/${characterId}`)
+            .then(res => res.json())
+            .then(data => {
+                setCharacter(data)
+            })
+
+    }, [characterId])
 
     return (
-        <h1>Character Page</h1>
+        <>
+            <h1>Character Page</h1>
+            <h2>{character.name}</h2>
+        </>
     )
 }
