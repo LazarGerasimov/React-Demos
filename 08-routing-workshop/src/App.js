8d9e4ea681d2a6ad686ae5f244115ab49b0326d4
Laz <lazgerasimov@gmail.com>
2023-03-21 11:00:39 +0000

    add Home comp

--- 08-routing-workshop/src/App.js
+++ 08-routing-workshop/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
 
 function App() {
     return (
         <div id="box">
             <Header />
-
+            <main id="main-content">
+                <Home />
+            </main>
         </div>
 
     );
