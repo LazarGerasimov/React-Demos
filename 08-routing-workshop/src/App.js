6dda344dcdc6bb2f23480e1f12a89e40a9eea1b1
Laz <lazgerasimov@gmail.com>
2023-03-21 11:07:55 +0000

    add Catalog/Create/Login/Register

--- 08-routing-workshop/src/App.js
+++ 08-routing-workshop/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { Catalog } from './components/Catalog/Catalog';
+import { CreateGame } from './components/CreateGame/CreateGame';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
 
 function App() {
     return (
         <div id="box">
             <Header />
             <main id="main-content">
-                <Home />
+                {/* <Home /> */}
+                {/* <Login /> */}
+                {/* <Register /> */}
+                {/* <CreateGame /> */}
+                <Catalog />
             </main>
         </div>
 
