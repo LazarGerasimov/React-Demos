8ec1d0ae07ed19a26dd35853f1bd4806b7732497
Laz <lazgerasimov@gmail.com>
2023-03-21 11:19:09 +0000

    add main routes

--- 08-routing-workshop/src/App.js
+++ 08-routing-workshop/src/App.js
@@ -6,16 +6,25 @@ import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
     return (
         <div id="box">
             <Header />
             <main id="main-content">
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create-game' element={<CreateGame />} />
+                    <Route path='/catalog' element={<Catalog />} />
+                </Routes>
                 {/* <Home /> */}
                 {/* <Login /> */}
                 {/* <Register /> */}
                 {/* <CreateGame /> */}
-                <Catalog />
+                {/* <Catalog /> */}
             </main>
         </div>
 
