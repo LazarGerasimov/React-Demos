b3a5c0ed00705817f51bc4f20e6b85a056125327
Laz <lazgerasimov@gmail.com>
2023-03-22 10:58:11 +0000

    add form vlaues

--- 08-routing-workshop/src/components/CreateGame/CreateGame.js
+++ 08-routing-workshop/src/components/CreateGame/CreateGame.js
@@ -13,7 +13,6 @@ export const CreateGame = ({
         maxLevel: null,
         imageUrl: '',
         summary: '',
-        
     });
 
     return (
@@ -23,19 +22,19 @@ export const CreateGame = ({
 
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter game title..." />
+                    <input value={values.title} type="text" id="title" name="title" placeholder="Enter game title..." />
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" placeholder="Enter game category..." />
+                    <input value={values.category} type="text" id="category" name="category" placeholder="Enter game category..." />
 
                     <label htmlFor="levels">MaxLevel:</label>
-                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
+                    <input value={values.maxLevel} type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                    <input value={values.imageUrl} type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
+                    <textarea name="summary" id="summary" value={values.summary}></textarea>
                     <input className="btn submit" type="submit" value="Create Game" />
                 </div>
             </form>
