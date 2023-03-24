e79f21d4887fb2a406395d7e47a4dbabbf1d8b55
Laz <lazgerasimov@gmail.com>
2023-03-24 13:52:36 +0000

    add useEffect/useState

--- 09-react-hooks/src/App.js
+++ 09-react-hooks/src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
 
 
@@ -6,7 +7,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 
+const baseUrl = 'http://localhost:3030/jsonstore/todos';
+
 function App() {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch(`${baseUrl}`)
+            .then(response => response.json())
+            .then(result => {
+                setTodos(Object.values(result));
+            })
+    }, [])
+
     return (
         <>
         <Header />
