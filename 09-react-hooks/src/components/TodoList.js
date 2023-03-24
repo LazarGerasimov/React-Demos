eba09bba6cd0d9ce2a891c026f7853fdf73ee51c
Laz <lazgerasimov@gmail.com>
2023-03-24 13:23:57 +0000

    add server

---
+++ 09-react-hooks/src/components/TodoList.js
@@ -0,0 +1,21 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+export const TodoList = () => {
+    return (
+        <div style={{ width: '30%', margin: '10px auto' }}>
+            <h1>Todo List</h1>
+            <ListGroup >
+                <ListGroup.Item action>
+                    Link 1
+                </ListGroup.Item>
+                <ListGroup.Item action>
+                    Link 2
+                </ListGroup.Item>
+                <ListGroup.Item action>
+                    This one is a button
+                </ListGroup.Item>
+            </ListGroup>
+        </div>
+    )
+}
