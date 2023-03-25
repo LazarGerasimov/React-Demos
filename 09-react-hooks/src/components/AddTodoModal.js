90a8c11ea8481eef9b28a59417c0b93e793dba88
Laz <lazgerasimov@gmail.com>
2023-03-25 13:19:30 +0000

    amend form

--- 09-react-hooks/src/components/AddTodoModal.js
+++ 09-react-hooks/src/components/AddTodoModal.js
@@ -1,33 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import { useRef } from 'react';
 
 
 export const AddTodoModal = () => {
 
-    const submitRef = useRef();
-
     return (
-        <Modal show={true}>
-            <Modal.Header closeButton>
-                <Modal.Title>Add Todo</Modal.Title>
-            </Modal.Header>
+        <Form>
+            <Modal show={true}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Todo</Modal.Title>
+                </Modal.Header>
 
-            <Modal.Body>
-                <Form>
+                <Modal.Body>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Todo</Form.Label>
                         <Form.Control type="text" placeholder="Add Todo here.." />
                     </Form.Group>
 
-                </Form>
-            </Modal.Body>
+                </Modal.Body>
 
-            <Modal.Footer>
-                <Button variant="secondary">Close</Button>
-                <Button variant="primary">Add</Button>
-            </Modal.Footer>
-        </Modal>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Modal.Footer>
+            </Modal>
+        </Form>
     )
 }
