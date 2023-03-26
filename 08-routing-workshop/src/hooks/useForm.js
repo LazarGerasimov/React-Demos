c8cc021a38c46c915c2eecd42da61cd791f33ba2
Laz <lazgerasimov@gmail.com>
2023-03-26 17:44:33 +0100

    create useForm (custom hook)

--- 08-routing-workshop/src/hooks/useForm.js
+++ 08-routing-workshop/src/hooks/useForm.js
@@ -1,4 +1,18 @@
+import { useState } from "react"
 
 
 
 
+export const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
+    return {
+        values,
+        changeHandler,
+        
+    }
+}
