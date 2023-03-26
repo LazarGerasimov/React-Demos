5f21b7e1cdb3fc11e7bbc29ffbefd2d170f07706
Laz <lazgerasimov@gmail.com>
2023-03-26 17:51:22 +0100

    upd login form

--- 08-routing-workshop/src/components/Login/Login.js
+++ 08-routing-workshop/src/components/Login/Login.js
@@ -1,11 +1,21 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
+import { useForm } from "../../hooks/useForm";
 
 
 
 export function Login() {
 
+    const LoginFormKeys = {
+        Email: 'email',
+        Password: 'password',
+    }
+
     const { onLoginSubmit } = useContext(AuthContext);
+    const { values, changeHandler } = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: '',
+    });
 
     return (
         <section id="login-page" className="auth">
@@ -14,10 +24,23 @@ export function Login() {
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input
+                        type="email"
+                        id="email"
+                        name={LoginFormKeys.Email}
+                        placeholder="Sokka@gmail.com"
+                        value={values[LoginFormKeys.Email]}
+                        onChange={changeHandler}
+                    />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input
+                        type="password"
+                        id="login-password"
+                        name={LoginFormKeys.Password}
+                        value={values[LoginFormKeys.Password]}
+                        onChange={changeHandler}
+                    />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
