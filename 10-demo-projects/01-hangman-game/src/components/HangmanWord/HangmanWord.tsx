c1c06877cda91aa3b5175a8a751876098d09ce73
Laz <lazgerasimov@gmail.com>
2023-04-30 19:32:39 +0100

    test span visibility

--- 10-demo-projects/01-hangman-game/src/components/HangmanWord/HangmanWord.tsx
+++ 10-demo-projects/01-hangman-game/src/components/HangmanWord/HangmanWord.tsx
@@ -1,11 +1,22 @@
 
 
 export function HangmanWord() {
+
     const word = 'test';
+    const guessedLetters = ['t'];
+
     return (
         <div style={{ display: 'flex', gap: '.25em', fontSize: '6em', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'monospace' }}>
             {word.split("").map((letter, index) => (
-                <span style={{borderBottom: '.1em solid black'}}>{letter}</span>
+                <span style={{ borderBottom: '.1em solid black' }} key={index}>
+                    <span style={{
+                        visibility: guessedLetters.includes(letter)
+                            ? 'visible'
+                            : 'hidden'
+                    }}>
+                        {letter}
+                    </span>
+                </span>
             ))}
         </div>
     )
