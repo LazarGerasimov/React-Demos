43cc6e893d284b8d669e12859dd37163289fe028
Laz <lazgerasimov@gmail.com>
2023-04-30 20:08:11 +0100

    upd HangmanWord comp

--- 10-demo-projects/01-hangman-game/src/App.tsx
+++ 10-demo-projects/01-hangman-game/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
                 Lose Win
             </div>
             <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-            <HangmanWord />
+            <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
             <div style={{ alignSelf: 'stretch' }}>
                 <HangmanKeyboard />
             </div>
