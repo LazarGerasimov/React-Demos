3511b541c3e81a471fa94f19258009f1449afe1f
Laz <lazgerasimov@gmail.com>
2023-04-30 19:18:58 +0100

    add HangmanWord styles

--- 10-demo-projects/01-hangman-game/src/components/HangmanWord/HangmanWord.tsx
+++ 10-demo-projects/01-hangman-game/src/components/HangmanWord/HangmanWord.tsx
@@ -2,8 +2,8 @@
 
 export function HangmanWord() {
     return (
-        <div>
-            Hangman Word
+        <div style={{ display: 'flex', gap: '.25em', fontSize: '6em', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'monospace' }}>
+            TEST
         </div>
     )
 }
