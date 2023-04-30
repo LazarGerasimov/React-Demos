7f2c882543986aea3dfc6244ef493658084d51fd
Laz <lazgerasimov@gmail.com>
2023-04-30 21:02:01 +0100

    add 'Enter' key functionality

--- 10-demo-projects/01-hangman-game/src/App.tsx
+++ 10-demo-projects/01-hangman-game/src/App.tsx
@@ -5,12 +5,13 @@ import { HangmanDrawing } from "./components/HangmanDrawing/HangmanDrawing";
 import { HangmanWord } from "./components/HangmanWord/HangmanWord";
 import { HangmanKeyboard } from "./components/HangmanKeyboard/HangmanKeyboard";
 
+function getWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
 
 function App() {
 
-    const [wordToGuess, setWordToGuess] = useState(() => {
-        return words[Math.floor(Math.random() * words.length)]
-    });
+    const [wordToGuess, setWordToGuess] = useState(getWord);
 
     const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
@@ -43,6 +44,23 @@ function App() {
             document.removeEventListener('keypress', handler)
         }
     }, [guessedLetters]);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key
+
+            if (key !== 'Enter') return;
+            e.preventDefault();
+            setGuessedLetters([]);
+            setWordToGuess(getWord())
+        }
+
+        document.addEventListener('keypress', handler)
+
+        return () => {
+            document.removeEventListener('keypress', handler)
+        }
+    })
 
     return (
         <div
