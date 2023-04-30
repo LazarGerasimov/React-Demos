81d8e4e9a0b28c3fb4811128a45f0314073abf64
Laz <lazgerasimov@gmail.com>
2023-04-30 20:22:00 +0100

    add addGuessedLetter functionality, useCallback

--- 10-demo-projects/01-hangman-game/src/App.tsx
+++ 10-demo-projects/01-hangman-game/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import words from './resources/wordList.json'
 
 import { HangmanDrawing } from "./components/HangmanDrawing/HangmanDrawing";
@@ -16,7 +16,30 @@ function App() {
 
     const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter));
 
+    const addGuessedLetter = useCallback((letter: string) => {
+        if (guessedLetters.includes(letter)) return;
+        setGuessedLetters(currentLetters => [...currentLetters, letter]);
+    }, [guessedLetters]);
     // console.log(wordToGuess);
+
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+
+            const key = e.key
+
+            if (!key.match(/^[a-z]$/)) return;
+            
+            e.preventDefault();
+            addGuessedLetter(key);
+        }
+
+        document.addEventListener('keypress', handler)
+
+        return () => {
+            document.removeEventListener('keypress', handler)
+        }
+    }, [guessedLetters]);
 
     return (
         <div
