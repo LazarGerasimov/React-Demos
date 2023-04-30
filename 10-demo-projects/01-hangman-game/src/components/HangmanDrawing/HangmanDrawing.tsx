23457211379fac12157cabee22efabb7f564e443
Laz <lazgerasimov@gmail.com>
2023-04-30 19:11:37 +0100

    add LEFT_ARM element

--- 10-demo-projects/01-hangman-game/src/components/HangmanDrawing/HangmanDrawing.tsx
+++ 10-demo-projects/01-hangman-game/src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -35,12 +35,29 @@ const RIGHT_ARM = (
     }}></div>
 );
 
+const LEFT_ARM = (
+    <div style={{
+        width: '100px',
+        height: '10px',
+        background: 'black',
+        position: 'absolute',
+        top: '150px',
+        right: '10px',
+        rotate: '30deg',
+        transformOrigin: 'right bottom'
+    }}></div>
+);
+
 
 
 
 export function HangmanDrawing() {
     return (
         <div style={{ position: 'relative' }}>
+            {HEAD}
+            {BODY}
+            {LEFT_ARM}
+            {RIGHT_ARM}
             <div style={{ height: '50px', width: '10px', background: 'black', position: 'absolute', top: 0, right: 0 }}></div>
             <div style={{ height: '10px', width: '200px', background: 'black', marginLeft: '120px' }}></div>
             <div style={{ height: '400px', width: '10px', background: 'black', marginLeft: '120px' }}></div>
