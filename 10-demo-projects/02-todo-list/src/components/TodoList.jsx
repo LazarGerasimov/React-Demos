1645fb6bd1bb29b77faaa5dc8d790c2e669e6a4d
Laz <lazgerasimov@gmail.com>
2023-05-02 11:09:41 +0100

    format TodoList

--- 10-demo-projects/02-todo-list/src/components/TodoList.jsx
+++ 10-demo-projects/02-todo-list/src/components/TodoList.jsx
@@ -12,7 +12,12 @@ export function TodoList({
             {todos.length === 0 && "No Todos added yet"}
             {todos.map(todo => {
                 // return <TodoItem id={todo.id} title={todo.title} completed={todo.completed} key={todo.id}/>
-                return <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+                return <TodoItem
+                    {...todo}
+                    key={todo.id}
+                    toggleTodo={toggleTodo}
+                    deleteTodo={deleteTodo}
+                />
             })}
         </ul>
     )
