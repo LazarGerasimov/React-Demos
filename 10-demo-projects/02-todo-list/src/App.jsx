98e0a9e22a320c1ea3763e90c7ced956cd4bab58
Laz <lazgerasimov@gmail.com>
2023-05-02 10:03:34 +0100

    form scaffold completed

--- 10-demo-projects/02-todo-list/src/App.jsx
+++ 10-demo-projects/02-todo-list/src/App.jsx
@@ -1,11 +1,40 @@
-
+import './styles.css'
 
 
 export default function App() {
-  return (
-    <>
-    <h1>test1</h1>
-    <h1>test2</h1>
-    </>
-  )
+   return (
+      <>
+         <form className='new-item-form'>
+            <div className='form-row'>
+               <label htmlFor="item">New Item</label>
+               <input type="text" id='item'/>
+            </div>
+            <button className='btn'>Add</button>
+         </form>
+         <h1 className='header'>Todo List</h1>
+         <ul className='list'>
+            <li>
+               <label>
+                  <input type="checkbox" />
+                  Item 1
+               </label>
+               <button className='btn btn-danger'>Delete</button>
+            </li>
+            <li>
+               <label>
+                  <input type="checkbox" />
+                  Item 2
+               </label>
+               <button className='btn btn-danger'>Delete</button>
+            </li>
+            <li>
+               <label>
+                  <input type="checkbox" />
+                  Item 3
+               </label>
+               <button className='btn btn-danger'>Delete</button>
+            </li>
+         </ul>
+      </>
+   )
 }
