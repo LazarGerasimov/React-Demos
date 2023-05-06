7544dbe4d34982bce009c419a8b42b1d6e47fc60
Laz <lazgerasimov@gmail.com>
2023-05-06 18:41:26 +0100

    add useLocalStorage custom hook; add types;

--- 10-demo-projects/03-note-taking-app/src/App.tsx
+++ 10-demo-projects/03-note-taking-app/src/App.tsx
@@ -3,8 +3,36 @@ import { Navigate, Route, Routes } from "react-router";
 import { Container } from "react-bootstrap";
 import { NewNote } from "./components/NewNote";
 
+export type Note = {
+   id: string;
+} & NoteData;
+
+export type RawNote = {
+   id: string;
+}
+
+export type RawNoteData = {
+   title: string;
+   markdown: string;
+   tagIds: string[];
+};
+
+export type NoteData = {
+   title: string;
+   markdown: string;
+   tags: Tag[]; 
+};
+
+export type Tag = {
+   id: string;
+   label: string;
+}
+
 
 function App() {
+
+   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);   
+   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);   
 
    return (
       <Container className="my-4">
