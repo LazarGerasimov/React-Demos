45ea68466ad499b277332a48fa639203ed476eb6
Laz <lazgerasimov@gmail.com>
2023-05-09 13:21:16 +0100

    upd card body

--- 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
+++ 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
@@ -92,6 +92,10 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
                 <Card.Body>
                     <Stack gap={2} className="align-items-center justify-content-center h-100">
                         <span className="fs-5">{title}</span>
+                        {tags.length > 0 && (
+                            <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+                            </Stack>
+                        )}
                     </Stack>
                 </Card.Body>
             </Card>
