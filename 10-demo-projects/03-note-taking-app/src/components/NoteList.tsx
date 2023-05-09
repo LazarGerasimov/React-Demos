a6afa4b69291c7a01cfc8780b11145debd08ac5f
Laz <lazgerasimov@gmail.com>
2023-05-09 13:19:19 +0100

    add card body

--- 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
+++ 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
@@ -90,7 +90,9 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
         <>
             <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
                 <Card.Body>
-
+                    <Stack gap={2} className="align-items-center justify-content-center h-100">
+                        <span className="fs-5">{title}</span>
+                    </Stack>
                 </Card.Body>
             </Card>
         </>
