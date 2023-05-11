00a70de81cea566638f5eb5dd44a440f617ff3ed
Laz <lazgerasimov@gmail.com>
2023-05-11 14:14:37 +0100

    add Note comp buttons and styles

--- 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
+++ 10-demo-projects/03-note-taking-app/src/components/NoteList.tsx
@@ -91,7 +91,7 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
             <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
                 <Card.Body>
                     <Stack gap={2} className="align-items-center justify-content-center h-100">
-                        <span className="fs-5">{title}</span>
+                    <span className="fs-5">{title}</span>
                         {tags.length > 0 && (
                             <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
                                 {tags.map(tag => (
